21dad7d2caceb46bd09e108c36030c3c732b06ef
CaisanLi <18081007254@sina.cn>
2021-06-08 22:33:57 +0800

    feat：添加待完成组件

---
+++ uview-ui/components/u-footer/u-footer.vue
@@ -0,0 +1,18 @@
+<template>
+	<view class="u-footer">
+		<!-- 待UI给图片 -->
+	</view>
+</template>
+
+<script>
+	export default {
+		name: "u-footer",
+		props: {
+			
+		}
+	}
+</script>
+
+<style>
+
+</style>
