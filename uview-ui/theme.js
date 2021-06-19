978bc545a6cde3d7f6c51ba3147f6403687d86e8
CaisanLi <18081007254@sina.cn>
2021-06-19 22:26:11 +0800

    feat：调整主题色

--- uview-ui/theme.js
+++ uview-ui/theme.js
@@ -1,3 +1,8 @@
 export default {
-	primaryColor: '#2EE6DC', // #2979ff
+	primary: '#2EE6DC', // #2979ff
+	default: '#909399',
+	info: '#909399',
+	success: '#65D94B',
+	error: '#FF1919',
+	warning: '#FFAA00'
 }
